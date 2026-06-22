79ffb2c749f7002d8747f744c12e61327df7acf2
Manabendu-ai <rikugithub@gmail.com>
2026-06-22 17:23:25 +0530

    query parameters

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
+
+@app.get("/blog")
+def blogs(limit: int, published : bool, sort : Optional[int] = 1): # query parameters
+    # Get 10 published Blogs
+
+    return {
+        'data': {
+            'limit' : limit,
+            'published' : published
+        }
+    }
+
 @app.get("/blog/{id}")
-def get_blog_by_id(id):
+def get_blog_by_id(id: int):
     # calling the db for the specified blog id
     return {
         "data" : {
