990c4c0ce81f0af1ea58635774d076525f2fe72c
Manabendu-ai <rikugithub@gmail.com>
2026-06-22 16:58:43 +0530

    Lesson 1 : adding path parameters

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/blog/{id}")
+def get_blog_by_id(id):
+    # calling the db for the specified blog id
+    return {
+        "data" : {
+            "blog" : {
+                "id" : id,
+                "msg" : "Heyy hello"
+            }
+        }
+    }
 
 @app.get("/author")
 def author():
