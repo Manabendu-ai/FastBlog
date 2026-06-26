6d2ee763695f1b10f4d49f7b3a0967f3a7daca1b
Manabendu-ai <rikugithub@gmail.com>
2026-06-26 18:31:12 +0530

    Posting a data {blog}

--- blogPy/main.py
+++ blogPy/main.py
@@ -1,15 +1,34 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from typing import Optional
-from .models import Base
-from .database import engine
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from .models import Base, Blog as BlogM
+from .database import engine, SessionLocal
 from .schemas import Blog
 app = FastAPI()
 
 Base.metadata.create_all(engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 @app.post('/blog/create/')
-def create_blog(blog : Blog):
-    return blog
+def create_blog(blog : Blog, db : Session = Depends(get_db)):
+    new_blog = BlogM(
+        title=blog.title,
+        author= blog.author,
+        body= blog.author,
+        published= blog.published
+    )
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
 @app.get("/blog")
 def blogs(limit: int, published : bool, sort : Optional[int] = 1): # query parameters
--- blogPy/schemas.py
+++ blogPy/schemas.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
-
 class Blog(BaseModel):
     title: str
     author: str
