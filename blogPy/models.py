6f307fad0c01620d06ce4bc018498da77ab03e61
Manabendu-ai <rikugithub@gmail.com>
2026-06-29 17:32:28 +0530

    SQL ALCHEMY RELATIONSHIP

--- blogPy/models.py
+++ blogPy/models.py
@@ -1,5 +1,7 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean, Text
+from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
 class Blog(Base):
     __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
@@ -7,9 +9,14 @@ class Blog(Base):
     author = Column(String(50))
     body = Column(Text)
     published = Column(Boolean)
+    user_email = Column(String(50), ForeignKey('users.email'))
+
+    owner = relationship('User', back_populates="blogs")
 
 class User(Base):
     __tablename__ = "users"
     email = Column(String(50), primary_key=True, index=True)
     name = Column(String(50))
     password = Column(String(255))
+
+    blogs = relationship('Blog', back_populates="owner")
